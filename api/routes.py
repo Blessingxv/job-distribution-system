fc258ffe2adc2e67b497722d830f7c5bfe3a0edb
Blessing <blessingtlangelani95@gmail.com>
2026-08-19 22:39:52 +0200

    Add GET /api/v1/jobs/{job_id} and GET /api/v1/jobs/{job_id}/result endpoints

--- api/routes.py
+++ api/routes.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from uuid import UUID
+
 from models.database import SessionLocal 
 from models.job import Job
 from api.schemas import JobCreateRequest, JobResponse
@@ -32,3 +34,51 @@ def create_job(request: JobCreateRequest, db: Session = Depends(get_db)):
         status = new_job.status
     )
 
+@router.get("/api/v1/jobs/{job_id}", response_model = JobResponse)
+def get_job(job_id: UUID, db: Session = Depends(get_db)):
+    job = db.get(Job, job_id)
+
+    if job is None:
+        raise HTTPException(
+            status_code = 404,
+            detail = "Job Not Found"
+        )
+
+    return JobResponse(
+        job_id = job.job_id,
+        job_type = job.job_type,
+        priority = job.priority,
+        status = job.status
+    )
+
+@router.get("/api/v1/jobs/{job_id}/result", response_model = JobResponse)
+def get_job_result(job_id: UUID, db: Session = Depends(get_db)):
+    job = db.get(Job, job_id)
+
+    if not job:
+        raise HTTPException(
+            status_code = 404,
+            detail = "Job Not Found"
+        )
+    elif job.status == JobStatus.PENDING.value:
+        raise HTTPException(
+            status_code = 409,
+            detail = "Conflict: Job is still in the queue"
+        )
+    elif job.status == JobStatus.RUNNING.value:
+        raise HTTPException(
+            status_code = 409,
+            detail = "Conflict: Job is still running"
+        )
+    elif job.status != JobStatus.COMPLETED.value:
+        raise HTTPException(
+            status_code = 409,
+            detail = "Job Failed"
+        )
+    return JobResponse(
+        job_id = job.job_id,
+        job_type = job.job_type,
+        priority = job.priority,
+        status = job.status
+    )
+    
